9d5ab85c7a35385c2f37c5c48e97998f07065d10
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-08-19 23:30:15 +0530

    Create 05_Pattern.c

---
+++ 05_Pattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int row = 5;
+    int col = 9;
+    
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            if(j >= i && j <= (col-1)-i) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+*********
+ ******* 
+  *****  
+   ***   
+    *  
