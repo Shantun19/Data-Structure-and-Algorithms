0bb1a9336222159a615d632b93954a53a954bd2d
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-08-19 23:06:05 +0530

    Create 01_Pattern.c

---
+++ 01_Pattern.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int row = 5;
+    int col = 5;
+    
+    for(int i=0; i<row; i++) {
+        for(int j=0; j<col; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+*/
