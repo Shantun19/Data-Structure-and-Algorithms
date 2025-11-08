632f3d002f851c60751f3cce7f0e47effaf57f8a
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-08 21:53:34 +0530

    Update 04_Pattern.c

--- 04_Pattern.c
+++ 04_Pattern.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
+import java.util.*;
 
-int main() {
-    int row = 5;
-    int col = 9;
-    
-    for(int i=0; i<row; i++) {
-        for(int j=0; j<col; j++) {
-            if(j >= 4-i && j <= 4+i) printf("*");
-            else printf(" ");
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = 5;
+        int cols = 9;
+        
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+              if(j >= (rows-1)-i && j <= (rows-1)+i) System.out.print("*");
+              else System.out.print(" ");
+            }
+            System.out.println();
         }
-        printf("\n");
     }
-
-    return 0;
 }
 
     *    
