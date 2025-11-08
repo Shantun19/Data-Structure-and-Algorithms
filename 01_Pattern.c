2060e477905a87185febcf533215ec5cfba5a38e
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-08 21:38:30 +0530

    Update 01_Pattern.c

--- 01_Pattern.c
+++ 01_Pattern.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
+import java.util.*;
 
-int main() {
-    int row = 5;
-    int col = 5;
-    
-    for(int i=0; i<row; i++) {
-        for(int j=0; j<col; j++) {
-            printf("*");
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = 5;
+        int cols = 5;
+        
+        for(int i=0; i<rows; i++) {
+            for(int j=0;j<cols; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        printf("\n");
+        
     }
-
-    return 0;
 }
 
 /*
