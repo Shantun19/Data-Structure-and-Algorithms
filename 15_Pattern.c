26a0704ca6b500852cb3e965d115c241a47e0522
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-14 00:09:32 +0530

    Update 15_Pattern.c

--- 15_Pattern.c
+++ 15_Pattern.c
@@ -1,25 +1,30 @@
-#include <stdio.h>
-
-int main() {
-    int rows = 5;
-    int col = 5;
-    
-    for(int i = 0; i < rows; i++) {
-        char temp = 'E' - i;
-        for(int j = 0; j < col; j++) {
-            if(j <= i) {
-                printf("%c", temp);
-                temp++;
-            }
-            else printf(" ");
-        }
-        printf("\n");
-    }
+public class Intro {
+	public static void main(String[] args) {
+		// define the rows of the grid
+		int rows = 5;
+		// define the columns of the grid.
+		int cols = 5;
+		
+		// traverse through the each row and column
+		for(int i=0; i<rows; i++) {
+			// getting character value based on the index 
+			char ch = (char) ('E' - i); // starting character for each row
+			for(int j=0; j<cols; j++) {
+				if(j <= i) {
+					System.out.print(ch++);
+				}
+				
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}	
 }
 
-// output 
+/*
 E    
 DE   
 CDE  
 BCDE 
 ABCDE
+*/
