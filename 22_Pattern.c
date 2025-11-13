8cb453e508db52398e1b1e4a7b86f2e2dbd7bcff
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-14 01:40:01 +0530

    Update 22_Pattern.c

--- 22_Pattern.c
+++ 22_Pattern.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
+import java.util.*;
 
-int main() {
-   int rows = 5;
-   int cols = 9;
-   
-   for(int i=0; i<rows; i++) {
-       for(int j=0; j<cols; j++) {
-           if(i == 0 && j == (cols/2)) printf("*");
-           else if(i == (rows-1)) printf("*");
-           else if(j == 4-i || j ==4+i) printf("*");
-           else printf(" ");
-    
-       }
-       printf("\n");
-   }
-
-    return 0;
+public class Intro {
+	public static void main(String[] args) {
+		int rows = 5;
+		int cols = 9;
+		
+		for(int i=0; i<rows; i++) {			
+			for(int j=0; j<cols; j++) {
+				// handling the last row 
+				if(i == (rows-1)) System.out.print("*");
+				else if(j == (rows-1-i) || j == (rows-1+i)) System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}	
 }
 
 // output 
