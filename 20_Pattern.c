f3611dfa83032ca03a0c4a3a1e89d9014f5022f0
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-14 01:15:26 +0530

    Update 20_Pattern.c

--- 20_Pattern.c
+++ 20_Pattern.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
-#include <math.h>
+import java.util.*;
 
-int main() {
-    int rows = 6;
-    int cols = 9;
-    
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            if(j >= i && j <= 3 + i) printf("*");
-            else printf(" ");
-        }
-        printf("\n");
-    }
+public class Intro {
+	public static void main(String[] args) {
+		int rows = 6;
+		int cols = 9;
+		
+		for(int i=0; i<rows; i++) {			
+			for(int j=0; j<cols; j++) {
+				if(j >= i && j <= 3 + i) System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}	
 }
 
-// output 
+/*
+
 ****     
  ****    
   ****   
    ****  
     **** 
      ****
+	 
+
+*/
