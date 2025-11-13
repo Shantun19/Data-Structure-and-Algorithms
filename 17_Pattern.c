5a6bbb79692861de4be9b11f12d02a263fbd567e
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-14 00:51:04 +0530

    Update 17_Pattern.c

--- 17_Pattern.c
+++ 17_Pattern.c
@@ -1,40 +1,25 @@
-#include <stdio.h>
-
-int main() {
-    int rows = 9;
-    int cols = 10;
-    
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            if(j < (cols/2)) {
-                if(i <= (rows/2)) {
-                    if(j <= i) printf("*");
-                    else printf(" ");
-                }
-                else {
-                    int k = i - (rows/2);
-                    if(j < 5-k) printf("*");
-                    else printf(" ");
-                }
-            }
-            else {
-                if(i <= (rows/2)) {
-                    if(j >= 9-i) printf("*");
-                    else printf(" ");
-                }
-                else {
-                    int k = i - (rows/2); // 5 - 4 = 1
-                    if(j >= 5+k) printf("*");
-                    else printf(" ");
-                }
-            }
-        }
-        printf("\n");
-    }
+public class Intro {
+	public static void main(String[] args) {
+		int rows = 10;
+		int cols = 10;
+		
+		for(int i=0; i<rows; i++) {			
+			for(int j=0; j<cols; j++) {
+				if(i < rows/2) {
+					if(j <= i || j >= 9-i) System.out.print("*");
+					else System.out.print(" ");
+				}
+				else {
+					if(j <= 8-i || j >= i+1) System.out.print("*");
+					else System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}	
 }
 
-
-// output 
+/*
 
 *        *
 **      **
@@ -45,3 +30,5 @@ int main() {
 ***    ***
 **      **
 *        *
+
+*/
