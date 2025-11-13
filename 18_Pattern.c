c23349b9852d50f883c62b83139ee7cf286b6fdd
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-14 01:02:17 +0530

    Update 18_Pattern.c

--- 18_Pattern.c
+++ 18_Pattern.c
@@ -1,21 +1,24 @@
-#include <stdio.h>
-
-int main() {
-    int rows = 4;
-    int cols = 4;
-    
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            if(i == 0 || i == (rows - 1)) printf("*");
-            else if(j == 0 || j == (cols - 1)) printf("*");
-            else printf(" ");
-        }
-        printf("\n");
-    }
+public class Intro {
+	public static void main(String[] args) {
+		int rows = 4;
+		int cols = 4;
+		
+		for(int i=0; i<rows; i++) {			
+			for(int j=0; j<cols; j++) {
+				if(i == 0 || i == rows-1) System.out.print("*");
+				else if(j == 0 || j == cols-1) System.out.print("*");
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}	
 }
 
-// output 
+/*
+
 ****
 *  *
 *  *
 ****
+
+*/
