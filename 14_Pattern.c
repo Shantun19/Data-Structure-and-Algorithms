f6131bf59d1f4c14e830be9190909424c750b4bd
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-13 23:51:45 +0530

    Update 14_Pattern.c

--- 14_Pattern.c
+++ 14_Pattern.c
@@ -1,30 +1,33 @@
-#include <stdio.h>
-
-int main() {
-    int rows = 4;
-    int col = 7;
-    
-    for(int i=0; i<rows; i++) {
-        char ch = '@';
-        for(int j=0; j<col; j++) {
-            if(j <= 3+i && j >= 3-i) {
-               if(j <= col/2) {
-                   ch++;
-                   printf("%c" , ch);
-               }
-               else {
-                   ch--;
-                   printf("%c" , ch);
-               }
-           }
-           else printf(" ");
-        }
-        printf("\n");
-    }
+public class Intro {
+	public static void main(String[] args) {
+		int rows = 4;
+		int cols = 7;
+		
+		for(int i=0; i<rows; i++) {
+			char ch = 'A';
+			for(int j=0; j<cols; j++) {
+				if(j >= 3-i && j <= 3+i) {
+					System.out.print(ch);
+					if(i != 0) {
+						if(j < (cols/2)) ch++;
+						else ch--;
+					}
+				}
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}	
 }
 
-// output 
+/*
    A   
   ABA  
  ABCBA 
 ABCDCBA
+
+0 -    4
+1 -   345
+2 -  12345
+3 - 0123456
+*/
