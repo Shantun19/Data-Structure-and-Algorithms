1570d6fc0b9359f02d3778aa5b835f0d3be9f902
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2025-11-13 23:16:35 +0530

    Update 11_Pattern.c

--- 11_Pattern.c
+++ 11_Pattern.c
@@ -1,29 +1,23 @@
-#include <stdio.h>
-
-int main() {
-    int rows = 5;
-    int col = 5;
-    
-    for(int i=0; i<rows; i++) {
-        // define a character 
-        char ch = 'A';
-        
-        for(int j=0; j<col; j++) {
-            if(j <= i) {
-                printf("%c " , ch);
-                ch++;
-            }
-            else {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
+public class Intro {
+	public static void main(String[] args) {
+		int rows = 5;
+		int cols = 5;
+		
+		for(int i=0; i<rows; i++) {
+			char ch = 'A';
+			for(int j=0; j<cols; j++) {
+				if(j <= i) System.out.print(ch++);
+				else System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}	
 }
 
-// output 
+/* output 
 A     
 A B    
 A B C   
 A B C D  
 A B C D E
+*/
